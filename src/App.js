4cc77feef59d7a11dc02a29e7cfa443de895e725
pjk3665@naver.com <pjk3665@naver.com>
2024-11-16 18:33:43 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     return (
         <div className="App">
           <h1 className="title">무인 우산 대여 시스템</h1>
-          <p className="description">우산이 필요하신가요? 편하게 우산을 빌려보세요</p>
+          <p className="description">우산이 필요하신가요? 편하게 우산을 빌려보세요.</p>
         </div>
       );
     }
