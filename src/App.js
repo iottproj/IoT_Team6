6c54a5c89cc8c677e16623bb1029cb108f04b1d5
pjk3665@naver.com <pjk3665@naver.com>
2024-11-16 18:31:26 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+          <h1 className="title">무인 우산 대여 시스템</h1>
+          <p className="description">우산이 필요하신가요? 편하게 우산을 빌려보세요</p>
+        </div>
+      );
+    }
 
 export default App;
