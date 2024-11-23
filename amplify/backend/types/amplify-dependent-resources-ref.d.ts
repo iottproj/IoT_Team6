c3a2073538e1ee41b1a835f8e98e212ff7e60191
cqb0709 <85153596+cqb0709@users.noreply.github.com>
2024-11-23 22:34:24 +0900

    add auth, installed @ui-react

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "iotteam6gen13cd5f954": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
