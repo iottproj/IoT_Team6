7c54910673e3012446b5cde37a74103980999c92
cqb0709 <85153596+cqb0709@users.noreply.github.com>
2024-11-25 16:27:04 +0900

    edit backends

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -10,6 +10,7 @@ export type AmplifyDependentResourcesAttributes = {
     "iotteam6gen13cd5f954": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "GoogleWebClient": "string",
       "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
@@ -21,6 +22,13 @@ export type AmplifyDependentResourcesAttributes = {
   },
   "function": {
     "UserInfoCRUD": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
+    "iotteam6gen13cd5f954CustomMessage": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
       "LambdaExecutionRoleArn": "string",
